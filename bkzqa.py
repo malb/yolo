a43dccfdcf395f4f64bff3e7bc00b38062db00e0
Martin R. Albrecht <martinralbrecht@googlemail.com>
2016-12-21 11:27:04 +0100

    WIP for BKZQA

--- bkzqa.py
+++ bkzqa.py
@@ -3,15 +3,24 @@
 BKZ 2.0 variant which ensures that the basis quality does not decrease.
 """
 
-from fpylll.algorithms.bkz_stats import dummy_tracer
+from fpylll.algorithms.bkz_stats import BKZTreeTracer, dummy_tracer
+import time
+import copy
 from fpylll.algorithms.bkz2 import BKZReduction as BKZ2
-# from fpylll.algorithms.bkz2 import BKZReduction as BKZ1
-from fpylll import BKZ, IntegerMatrix, Enumeration, EnumerationError
-from fpylll.util import gaussian_heuristic
+from fpylll import BKZ, IntegerMatrix, Enumeration, EnumerationError, LLL
+from fpylll.util import gaussian_heuristic, set_random_seed
 
 
 class BKZReduction(BKZ2):
     def copy_block(self, kappa, block_size):
+        """
+        Append a copy of the block from ``kappa`` to ``kappa + block_size`` to the end of the
+        matrix.
+
+        :param kappa: start index
+        :param block_size: number of vectors to append
+
+        """
         for i in range(block_size):
             self.M.create_row()
 
@@ -20,38 +29,88 @@ class BKZReduction(BKZ2):
                 self.M.row_addmul(self.M.d-block_size+i, kappa+i, 1)
 
     def delete_copy_block(self, kappa, block_size, restore):
+        """Delete copy of block from ``kappa`` to ``kappa + block_size``
+
+        If ``restore`` is ``True`` then copy it back to the block starting at``kappa``
+
+        :param kappa: start index.
+        :param block_size: number of vectors to consider
+        :param restore: copy block back
+
+        """
         if restore:
             for i in range(block_size):
-                # this implement a swap
+                # this implements a swap
                 self.M.move_row(self.M.d-block_size+i, kappa+i)
                 self.M.move_row(kappa+i+1, self.M.d-block_size+i)
+
         for i in range(block_size):
             self.M.remove_last_row()
 
-    # def svp_preprocessing(self, kappa, block_size, param, tracer=dummy_tracer):
-    #     clean = True
+    def __call__(self, params, min_row=0, max_row=-1):
+        """Run the BKZ algorithm with parameters `param`.
 
-    #     clean &= BKZ1.svp_preprocessing(self, kappa, block_size, param, tracer)
-
-    #     for preproc in param.strategies[block_size].preprocessing_block_sizes:
-    #         prepar = param.__class__(block_size=preproc, strategies=param.strategies,
-    #                                  flags=BKZ.GH_BND|BKZ.BOUNDED_LLL)
-    #         clean &= self.tour(prepar, kappa, kappa + block_size)
-
-    #     return clean
-
-    def svp_reduction(self, kappa, block_size, param, tracer=dummy_tracer):
-        """
-
-        :param kappa:
-        :param block_size:
-        :param params:
-        :param tracer:
+        :param params: BKZ parameters
+        :param min_row: start processing in this row
+        :param max_row: stop processing in this row (exclusive)
 
         """
+        tracer = BKZTreeTracer(self, verbosity=params.flags & BKZ.VERBOSE)
 
-        self.lll_obj.size_reduction(0, kappa+1)
-        old_first, old_first_expo = self.M.get_r_exp(kappa, kappa)
+        auto_abort = BKZ.AutoAbort(self.M, self.A.nrows)
+        cputime_start = time.clock()
+
+        with tracer.context("lll"):
+            self.lll_obj()
+
+        i = 0
+        while True:
+            with tracer.context("tour", i):
+                self.tour(params, min_row, max_row, tracer, top_level=True)
+            i += 1
+            if params.block_size >= self.A.nrows:
+                break
+            if auto_abort.test_abort():
+                break
+            if (params.flags & BKZ.MAX_LOOPS) and i >= params.max_loops:
+                break
+            if (params.flags & BKZ.MAX_TIME) and time.clock() - cputime_start >= params.max_time:
+                break
+
+        self.trace = tracer.trace
+
+    def tour(self, params, min_row=0, max_row=-1, tracer=dummy_tracer, top_level=False):
+        """One BKZ loop over all indices.
+
+        :param params: BKZ parameters
+        :param min_row: start index ≥ 0
+        :param max_row: last index ≤ n
+
+        :returns: ``True`` if no change was made and ``False`` otherwise
+        """
+        if max_row == -1:
+            max_row = self.A.nrows
+
+        for kappa in range(min_row, max_row-2):
+            block_size = min(params.block_size, max_row - kappa)
+            self.svp_reduction(kappa, block_size, params, tracer, top_level=top_level)
+
+    def svp_preprocessing(self, kappa, block_size, param, tracer=dummy_tracer):
+
+        with tracer.context("lll"):
+            # run LLL between kappa and kappa + block_size
+            self.lll_obj(kappa, kappa, kappa + block_size, kappa)
+
+        for preproc in param.strategies[block_size].preprocessing_block_sizes:
+            prepar = param.__class__(block_size=preproc, strategies=param.strategies,
+                                     flags=BKZ.GH_BND|BKZ.BOUNDED_LLL)
+            self.tour(prepar, kappa, kappa + block_size, tracer=tracer)
+
+    def svp_reduction(self, kappa, block_size, param, tracer=dummy_tracer, top_level=False):
+        if top_level:
+            # do a full LLL up to kappa + block_size
+            with tracer.context("lll"):
+                self.lll_obj(0, kappa, kappa+block_size, 0)
 
         remaining_probability, rerandomize = 1.0, False
 
@@ -84,7 +143,7 @@ class BKZReduction(BKZ2):
                     solution, max_dist = enum_obj.enumerate(kappa, kappa + block_size, radius, expo,
                                                             pruning=pruning.coefficients)[0]
                 with tracer.context("postprocessing"):
-                    self.svp_postprocessing(kappa, block_size, solution, tracer=tracer)
+                    self.svp_postprocessing(kappa, block_size, solution, tracer=tracer, top_level=top_level)
                     if rerandomize:
                         self.delete_copy_block(kappa, block_size, restore=False)
                 rerandomize = False
@@ -92,26 +151,63 @@ class BKZReduction(BKZ2):
             except EnumerationError:
                 with tracer.context("postprocessing"):
                     if rerandomize:
+                        # restore block, TODO don't do this unconditionally
                         self.delete_copy_block(kappa, block_size, restore=True)
                 rerandomize = True
 
             remaining_probability *= (1 - pruning.probability)
 
-        self.lll_obj.size_reduction(0, kappa+1)
-        new_first, new_first_expo = self.M.get_r_exp(kappa, kappa)
+    def svp_postprocessing(self, kappa, block_size, solution, tracer, top_level):
+        """Insert SVP solution into basis and LLL reduce.
 
-        clean = old_first <= new_first * 2**(new_first_expo - old_first_expo)
-        return clean
+        :param solution: coordinates of an SVP solution
+        :param kappa: current index
+        :param block_size: block size
+        :param tracer: object for maintaining statistics
+
+        """
+        if solution is None:
+            return
+
+        nonzero_vectors = len([x for x in solution if x])
+
+        if nonzero_vectors == 1:
+            first_nonzero_vector = None
+            for i in range(block_size):
+                if abs(solution[i]) == 1:
+                    first_nonzero_vector = i
+                    break
+
+            self.M.move_row(kappa + first_nonzero_vector, kappa)
+            with tracer.context("lll"):
+                self.lll_obj.size_reduction(kappa, kappa + first_nonzero_vector + 1)
+        else:
+            d = self.M.d
+            self.M.create_row()
+
+            with self.M.row_ops(d, d+1):
+                for i in range(block_size):
+                    self.M.row_addmul(d, kappa + i, solution[i])
+
+            self.M.move_row(d, kappa)
+            with tracer.context("lll"):
+                self.lll_obj(kappa, kappa, kappa + block_size + 1)
+            self.M.move_row(kappa + block_size, d)
+            self.M.remove_last_row()
 
 
-import copy
+set_random_seed(1337)
 n = 120
-block_size = 68
+block_size = 60
 A = IntegerMatrix.random(n, "qary", k=n//2, bits=36)
 bkz = BKZReduction(copy.copy(A))
 bkz(BKZ.Param(block_size=block_size, max_loops=2, strategies=BKZ.DEFAULT_STRATEGY, flags=BKZ.MAX_LOOPS|BKZ.VERBOSE))
+
+trace = bkz.trace
+print trace.report()
 
 print
 
 bkz = BKZ2(copy.copy(A))
 bkz(BKZ.Param(block_size=block_size, max_loops=2, strategies=BKZ.DEFAULT_STRATEGY, flags=BKZ.MAX_LOOPS|BKZ.VERBOSE))
+
